300b7a69f15d3a683bfa3eacc896ed35caac9eeb
thomas.laux <laux.thomas93@gmail.com>
2022-05-04 21:05:30 +0200

    Created naive Garlic AoE Attack

---
+++ Assets/Scripts/Garlic.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Garlic : MonoBehaviour
+{
+    public float attackSpeed = 10f;
+    public float maxDamageCooldown;
+    public float damageCooldown;
+    public float strength = 5;
+
+    private void Start()
+    {
+        maxDamageCooldown = 1 / attackSpeed;
+    }
+
+    private void Update()
+    {
+        damageCooldown -= Time.deltaTime;
+    }
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            if (damageCooldown <= 0)
+            {
+                Debug.Log("Garlic hit");
+                other.GetComponent<EnemyController>().TakeDamage(strength);
+                damageCooldown = maxDamageCooldown;
+            }
+        }
+    }
+}
